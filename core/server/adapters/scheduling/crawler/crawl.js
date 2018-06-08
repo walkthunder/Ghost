e7505efd0d58f611af297d023cd425de9e662024
zhengpenghui <zhengpenghui@qingting.fm>
2018-06-08 19:00:06 +0800

    feat(crawl): Add link crawler

--- core/server/adapters/scheduling/crawler/crawl.js
+++ core/server/adapters/scheduling/crawler/crawl.js
@@ -1,6 +1,11 @@
 let debug = require('ghost-ignition').debug('crawler');
+let _ = require('lodash');
+let uuid = require('uuid');
+let ObjectId = require('bson-objectid');
 let crawllinkAPI = require('../../../api/crawllinks');
 let crawlsiteAPI = require('../../../api/crawlsites');
+let postsAPI = require('../../../api/posts');
+let models = require('../../../models');
 let puppeter = require('./puppeter');
 /**
  * Crawler task engine component
@@ -16,10 +21,102 @@ const updateLog = function (options) {
     return crawlsiteAPI.edit({crawlsites: [{status: 'done'}]}, options);
 };
 
+function markdownToMobiledoc(content) {
+    var mobiledoc = {
+        version: '0.3.1',
+        markups: [],
+        atoms: [],
+        cards: [
+            ['markdown', {
+                cardName: 'markdown',
+                markdown: content || ''
+            }]
+        ],
+        sections: [[10, 0]]
+    };
+    return JSON.stringify(mobiledoc);
+}
+
+function createPost(overrides) {
+    overrides = overrides || {};
+
+    var newObj = _.cloneDeep(overrides),
+        mobiledocObj;
+
+    if (!newObj.mobiledoc) {
+        newObj.mobiledoc = markdownToMobiledoc('## markdown');
+    }
+
+    if (!newObj.html) {
+        mobiledocObj = JSON.parse(newObj.mobiledoc);
+        newObj.html = mobiledocObj.cards && mobiledocObj.cards[0][1].markdown;
+    }
+
+    return models.User.findOne({
+        slug: 'ghost'
+    }).then(function (collector = {}) {
+        let postData = _.defaults(newObj, {
+            id: ObjectId.generate(),
+            uuid: uuid.v4(),
+            title: 'iInsert002',
+            status: 'published',
+            feature_image: null,
+            featured: false,
+            page: false,
+            author_id: collector.id || '1',
+            updated_at: new Date(),
+            updated_by: collector.id || '1',
+            created_at: new Date(),
+            created_by: collector.id || '1',
+            published_at: new Date(),
+            published_by: collector.id || '1',
+            visibility: 'public'
+        });
+        return postsAPI.add(postData)
+            .then(function onModelResponse(model) {
+                var post = model.toJSON();
+                return {posts: [post]};
+            });
+    });
+}
+const html = '<section><body><h2>H2 Title</h2><p>body content</p></body></section>';
+const mockData = {
+    title: 'iInsert003',
+    slug: 'html-ipsum',
+    html,
+    mobiledoc: markdownToMobiledoc(html),
+    published_at: new Date(),
+    custom_excerpt: html,
+    feature_image: 'https://casper.ghost.org/v1.0.0/images/writing.jpg'
+};
+
 const crawl = function (log) {
     debug('crawl log - ', log);
     return puppeter(log)
 };
+
+const _crawlLink = function (link) {
+    debug('_crawlLink', link)
+    return puppeter(link, 'link')
+}
+
+const crawlLinks = function (links = []) {
+    return links.reduce((res, link) => {
+        return res.then((finalRespArray) => {
+            return new Promise((resolve) => {
+                return _crawlLink(link)
+                    .then(linkResp => {
+                        finalRespArray.push(linkResp)
+                        return resolve(finalRespArray)
+                    })
+                    .catch(err => {
+                        debug('crawl links error - ', err)
+                        return resolve(finalRespArray)
+                    })
+            })
+        })
+    }, Promise.resolve([]))
+}
 
 const formatCL = function (cl) {
     return cl ? {
@@ -125,5 +222,44 @@ module.exports = {
             .catch((err) => {
                 debug(err);
             });
+    },
+    linkCrawler: function () {
+        debug('link engine start')
+        // Load link
+        let crawllinks = []
+        return crawllinkAPI.browse({
+            status: 'pending'
+        })
+            .then(resp => {
+                if (resp && resp.crawllinks) {
+                    crawllinks = resp.crawllinks
+                }
+            })
+            .then(() => {
+                return crawlLinks(crawllinks)
+            })
+            .then((links) => {
+                debug('links crawlled back with - ', links)
+                let updateDBPromises = links.map(link => {
+                    return createPost({
+                        title: link.title,
+                        slug: '',
+                        status: 'draft',
+                        published_at: new Date(),
+                        mobiledoc: markdownToMobiledoc(link.content),
+                        html: link.content
+                    })
+                })
+                return Promise.all(updateDBPromises)
+                    .then(() => {
+                        console.log()
+                    })
+            })
+            .catch(err => {
+                debug('linkCrawler error - ', err)
+            })
+        // crawl
+        // generate post
+        // update link record
     }
 };
--- core/server/adapters/scheduling/crawler/puppeter.js
+++ core/server/adapters/scheduling/crawler/puppeter.js
@@ -2,11 +2,11 @@ let puppeteer = require('puppeteer');
 let auth = require('./auth.js');
 let debug = require('ghost-ignition').debug('puppeter');
 
-module.exports = function (log) {
+module.exports = function (log, type = 'site') {
     if (!log) {
         return Promise.reject();
     }
-    const url = log.res_url;
+    const url = (type === 'link') ? log.uri : log.res_url
     const query = log.query_rule;
     const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'
     let brow;
@@ -20,21 +20,27 @@ module.exports = function (log) {
                 .then(() => {
                     return page.goto(url)
                 })
-                .then((resp) => {
+                .then(() => {
                     return auth(page).then(() => {
                         return page.pdf({path: `./page.${url}.pdf`,  format: 'A4'})
                     })
                 })
-                .then((res) => {
+                .then(() => {
+                    /* global document, window */
                     return page.evaluate((sel) => {
-                        return [...(document.querySelectorAll(sel) || [])].map(item => {
-                            return item.href;
-                        })
+                        if (type === 'site') {
+                            return [...(document.querySelectorAll(sel) || [])].map(item => item.href) // eslint-disable-line arrow-body-style
+                        }
+                        let target = document.querySelectorAll(sel)
+                        return {
+                            title: document.title || window.location.href,
+                            content: target.innerHTML
+                        }
                     }, query)
                 })
         })
         .then((res) => {
-            console.log('final res - ', res)
+            debug('final res - ', res)
             brow.close();
             return res;
         });
